638a820cffb9715aa8245377bdbc7d4c2700f24d
yna87 <yna.sakana@gmail.com>
2026-01-12 19:11:10 +0900

    chore: add pinia

--- apps/frontend/src/main.ts
+++ apps/frontend/src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import './style.css'
 import App from './App.vue'
 import router from './router'
@@ -6,7 +7,9 @@ import ui from '@nuxt/ui/vue-plugin'
 import { VueQueryPlugin } from '@tanstack/vue-query'
 
 const app = createApp(App)
+const pinia = createPinia()
 
+app.use(pinia)
 app.use(ui)
 app.use(router)
 app.use(VueQueryPlugin)
